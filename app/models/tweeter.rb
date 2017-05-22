4017bd515780b33869cd1043fbec0a0d5278d54e
Daniel Lee <dlee@sheetmusicplus.com>
2017-05-21 18:28:19 -0700

    Add validations for Tweeter#uid

--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -1,4 +1,4 @@
 class Tweeter < ApplicationRecord
-  validates_presence_of :screen_name, :name
-  validates_uniqueness_of :screen_name
+  validates_presence_of :uid, :screen_name, :name
+  validates_uniqueness_of :uid, :screen_name
 end
--- spec/models/tweeter_spec.rb
+++ spec/models/tweeter_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Tweeter, type: :model do
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:screen_name) }
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:screen_name) }
+    it { is_expected.to validate_presence_of(:uid) }
     it { is_expected.to validate_uniqueness_of(:screen_name) }
+    it { is_expected.to validate_uniqueness_of(:uid) }
   end
 end
