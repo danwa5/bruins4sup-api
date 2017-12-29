0eff6d82eebf037eaa5a39fc0c9c8a3c54e9cc1e
Daniel Lee <dlee@sheetmusicplus.com>
2017-12-28 22:13:47 -0800

    Add sessions specs and update README

--- README.md
+++ README.md
@@ -20,6 +20,12 @@ $ bundle check || bundle install
 * Generate your consumer key and secret
 * Set your TWITTER_API_KEY and TWITTER_API_SECRET environment variables
 
+### Twitter Authentication
+
+* Sign in to https://apps.twitter.com
+* Set the callback URL for your app
+* On your app navigate to `/auth/twitter/callback?screen_name=YOUR_TWITTER_HANDLE`
+
 ### Run Test Suite
 
 ```shell
---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Sessions', type: :request do
+  describe 'GET /auth/twitter/callback' do
+    it 'returns the authenticated user' do
+      user = create(:user)
+      allow(User).to receive(:find_or_create_from_auth_hash).and_return(user)
+      expect(User).to receive(:find_or_create_from_auth_hash).once
+
+      get '/auth/:provider/callback'
+      json = JSON.parse(response.body)
+      
+      expect(response).to be_success
+      aggregate_failures 'response json' do
+        expect(json['id']).to eq(user.id)
+        expect(json['uid']).to eq(user.uid)
+        expect(json['name']).to eq(user.name)
+        expect(json['token']).to eq(user.token)
+        expect(json['secret']).to eq(user.secret)
+      end
+    end
+  end
+end
